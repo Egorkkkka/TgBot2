5635093e5ad7dc7848a29c31da71f1c965981cd5
Egorkkkka <egorlantsov@mail.ru>
2022-11-01 09:52:39 +0500

    dd

--- src/main/java/org/example/TestBot.java
+++ src/main/java/org/example/TestBot.java
@@ -1,14 +1,18 @@
 package org.example;
 
-import org.telegram.telegrambots.bots.DefaultAbsSender;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class TestBot extends DefaultAbsSender {
+public class TestBot extends TelegramLongPollingBot {
 
-    protected TestBot(DefaultBotOptions options) {
-        super(options);
+    @Override
+    public String getBotUsername() {
+        return "Urfu_Botik";
     }
 
     @Override
@@ -17,8 +21,26 @@ public class TestBot extends DefaultAbsSender {
     }
 
     public static void main(String[] args) throws TelegramApiException {
-        TestBot bot = new TestBot(new DefaultBotOptions());
-        bot.execute(SendMessage.builder().chatId("374927123123548").text("Hello from Java").build());
-        bot.execute(SendMessage.builder().chatId("374927123123548").text("Hello from Java").build());
+        TestBot bot = new TestBot();
+        TelegramBotsApi telegramBotApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotApi.registerBot(bot);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()){
+                try {
+                    execute(
+                            SendMessage.builder()
+                                    .chatId(message.getChatId().toString())
+                                    .text("Ты написал - " + message.getText())
+                                    .build());
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
